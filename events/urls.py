601b8a44b69550c5467425979d0772c35ba1e4e3
sarvanideekshitula <sarvanimini@gmail.com>
2017-06-17 12:39:28 +0530

    modified urls, views

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
-
+from events.views import Events
 
 urlpatterns = [
-    url(r'^Event/(?P<pk>[0-9]+)', Event, name='Event'),
+    url(r'^Event/(?P<pk>[0-9]+)', Events, name='Events'),
 ]
--- events/views.py
+++ events/views.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
+from events.models import Event
+
+
+def Events(request):
+    template = 'event.html'
+    object_list = Event.objects.all()
+    context = {'object_list': object_list}
+    return render(request, template, context)
